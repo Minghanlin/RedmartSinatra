eb5db489b4cbdeaba55d81c1792a03d9a7f4e494
Minghan Lin <cd>
2016-07-22 10:43:53 +0800

    configurations

--- app.rb
+++ app.rb
@@ -2,24 +2,23 @@ class RedmartSinatraApp < Sinatra::Base
   get '/' do
       erb "Hello World"
     end
+    
+# Restful resources
 
-  get '/about' do
-      erb "About Redmart"
+    get '/users' do
+      @users = User.all
+      erb :'users/index'
     end
 
+    get "/users/new" do
+      erb :'users/new'
+    end
 
-  get '/users' do
+    get '/users/:id' do
+      @user = User.find(params[:id])
+      erb :'users/show'
+    end
 
-  @users = User.all
-
-  erb :'users'
-  end
-
-  get '/users/:id' do
-    @user =
-    User.find(params[:id])
-    erb :'each_user'
-  end
-
-
-end
+    get "/users/:id/edit" do
+      erb :'users/edit'
+    end
